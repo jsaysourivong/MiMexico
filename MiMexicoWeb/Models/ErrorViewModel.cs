7f318a7358cffed2e0f2ae4e72a69b4001ee953d
Jonah Saysourivong <jsaysourivong@gmailcom>
2022-04-13 16:04:24 -0700

    Add project files.

---
+++ MiMexicoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiMexicoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
